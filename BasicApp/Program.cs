4ddf097496746774eba0719912b13ebe243bee5e
sangam chaudhary <sangamkushmi8@gmail.com>
2024-09-03 19:25:07 +0545

    learnt about datatypes

--- BasicApp/Program.cs
+++ BasicApp/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//OOP: Object oriented program
+//static vs dynamic
+//procedural vs oo vs functional 
+// imperative vs declerative
+
+
+// Datatype
+//number- integer,float
+
+byte PersonAge=45;
+short Student=2342;
+int CountryPopulation=26494504;
+long WeightofEarth=875636262323;
+
+//Floating point number: precision (accuracy)
+float Weightofelephant=14567.12121454654654644;
+double freezer price=123446546.7661253635656;
+decimal moleculeweight=46.774772656665565655;
+
+// Text
+Stringname collegename="nast";
+char Gender="m";
+//Bolean
+bool isAbesentToday="True";
+//custom
+DateTime Dob=DateTime.Now;
+
+var FullName="Sangam"
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine(PersonAge);
+
+
+// decimal
